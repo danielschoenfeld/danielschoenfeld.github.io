3327e10dcb0a7dcdb9ec288db4b7f1ebb895b748
danielschoenfeld <daniel-schoenfeld@arcor.de>
2021-10-22 18:26:47 +0200

    initial start

--- script/script.js
+++ script/script.js
@@ -2,12 +2,32 @@
 console.log("Hello World")
 const slider = document.querySelector(".items");
 const slides = document.querySelectorAll(".item");
-const button = document.querySelectorAll(".button");
+const button_prev = document.querySelectorAll(".button-prev");
+const button_next = document.querySelectorAll(".button-next");
 
 let current = 0;
 let prev = 4;
 let next = 1;
 
+button_prev.addEventListener("click", function() {
+    console.log("Go to previous slide")
+    if(current-1 < 0){
+        gotoNum(slides.length -1)
+    }else{
+        gotoNum(current - 1)
+    }
+})
+
+button_next.addEventListener("click", function () {
+    console.log("Go to next slide")
+    if (current + 1 > slides.length) {
+        gotoNum(0)
+    } else {
+        gotoNum(current + 1)
+    }
+})
+
+/*
 for (let i = 0; i < button.length; i++) {
     button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
 }
@@ -15,11 +35,12 @@ for (let i = 0; i < button.length; i++) {
 const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
 
 const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
-
-const gotoNum = number => {
-    current = number;
-    prev = current - 1;
-    next = current + 1;
+*/
+function gotoNum(number){
+    new_slide = number;
+    console.log(new_slide)
+    prev = new_slide - 1;
+    next = new_slide + 1;
 
     for (let i = 0; i < slides.length; i++) {
         slides[i].classList.remove("active");
@@ -35,7 +56,7 @@ const gotoNum = number => {
         prev = 4;
     }
 
-    slides[current].classList.add("active");
+    slides[current_slide].classList.add("active");
     slides[prev].classList.add("prev");
     slides[next].classList.add("next");
 }
